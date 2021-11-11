09695f78cc1753dcf9252c46401d7fe1f8ea5c6b
Vadim Constantin <vad.constantin@gmail.com>
2021-11-11 21:37:32 +0100

    watch list exo done

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,9 @@
 class Bookmark < ApplicationRecord
-  validates :movie_id, uniqueness: { scope: :list_id}
-  validates :comment, length: { minimum: 6 }
   belongs_to :movie
   belongs_to :list
+
+  validates :movie_id, uniqueness: { scope: :list_id }
+  validates :comment, length: { minimum: 6 }
+
+
 end
